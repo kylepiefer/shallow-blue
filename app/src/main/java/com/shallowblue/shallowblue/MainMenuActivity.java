bacd31649c7889c6d7f58209271b9c6e9097f279
kylepiefer <kylepiefer@gmail.com>
2016-03-16 00:19:26 -0500

    Make many additions to the UI and remove some unecessary files. Update existing files to match our requirements document

---
+++ app/src/main/java/com/shallowblue/shallowblue/MainMenuActivity.java
@@ -0,0 +1,33 @@
+package com.shallowblue.shallowblue;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+    public void startMultiplayerGame(View button) {
+        Intent startGameIntent = new Intent(getApplicationContext(), StartGameActivity.class);
+        startActivity(startGameIntent);
+    }
+
+    public void startSingleplayerGame(View button) {
+        Intent startGameIntent = new Intent(getApplicationContext(), StartGameActivity.class);
+        startActivity(startGameIntent);
+    }
+
+}
