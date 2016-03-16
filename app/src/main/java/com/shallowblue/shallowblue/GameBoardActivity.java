d0c2cd896cdf9438d11a9f437a7ae5ae31d81f6b
kylepiefer <kylepiefer@gmail.com>
2016-03-16 17:27:22 -0500

    Add board to multiplayer and add IDs for movement

--- app/src/main/java/com/shallowblue/shallowblue/GameBoardActivity.java
+++ app/src/main/java/com/shallowblue/shallowblue/GameBoardActivity.java
@@ -3,6 +3,7 @@ package com.shallowblue.shallowblue;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class GameBoardActivity extends AppCompatActivity {
 
