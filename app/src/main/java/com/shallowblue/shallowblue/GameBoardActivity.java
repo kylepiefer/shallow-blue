f77a0b14e5adcba2cd7b7573f9469979055ceb54
Austin bovee <abovee@wisc.edu>
2016-03-19 02:39:57 -0500

    Making buttons on GameBoard have click events

--- app/src/main/java/com/shallowblue/shallowblue/GameBoardActivity.java
+++ app/src/main/java/com/shallowblue/shallowblue/GameBoardActivity.java
@@ -3,7 +3,7 @@ package com.shallowblue.shallowblue;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
+import android.view.View;
 
 public class GameBoardActivity extends AppCompatActivity {
 
@@ -12,5 +12,25 @@ public class GameBoardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_board);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+
+    public void startinghelper(View v){
+
+    }
+
+    public void altmove(View v){
+
+    }
+
+    public void undomove(View v){
+
+    }
+
+    public void redomove(View v){
+
+    }
+
+    public void optionsScreen(View v){
+
     }
 }
