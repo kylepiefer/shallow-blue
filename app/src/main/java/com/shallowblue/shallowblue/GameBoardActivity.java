347a106a4166cbd37a1a6f7b1954419fa5b27a23
kofe <fkf2fkf2fkf2@126.com>
2016-04-06 17:30:59 -0500

    add apache connection function to single player mode

--- app/src/main/java/com/shallowblue/shallowblue/GameBoardActivity.java
+++ app/src/main/java/com/shallowblue/shallowblue/GameBoardActivity.java
@@ -2,6 +2,7 @@ package com.shallowblue.shallowblue;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -12,8 +13,22 @@ import android.widget.FrameLayout;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+
+
+//apache connection
 import java.util.ArrayList;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.apache.http.NameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
 import java.util.Map;
 
 public class GameBoardActivity extends AppCompatActivity {
@@ -306,11 +321,13 @@ public class GameBoardActivity extends AppCompatActivity {
     }
 
     public void startingHelper(View v){
-
+        httpStore hS = new httpStore();
+        hS.execute();
     }
 
     public void altMove(View v){
-
+        httpQuery hQ = new httpQuery();
+        hQ.execute();
     }
 
     public void undoMove(View v){
@@ -332,6 +349,75 @@ public class GameBoardActivity extends AppCompatActivity {
         verify.putString("activity","main");
         check.putExtra("next",verify);
         startActivity(check);
+    }
+
+    protected class httpStore extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... strs) {
+            String reply = null;
+            String temp1="";
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost storeVal = new HttpPost("http://shallowBlue");
+            ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+            nameValuePairs.add(new BasicNameValuePair("tag", "shallowBlue"));
+            nameValuePairs.add(new BasicNameValuePair("GameBoard", gameBoard.pack()));
+
+            try {
+                UrlEncodedFormEntity httpEntity = new UrlEncodedFormEntity(nameValuePairs);
+                storeVal.setEntity(httpEntity);
+                HttpResponse response = httpclient.execute(storeVal);
+                temp1 = EntityUtils.toString(response.getEntity());
+            }
+            catch (Exception e) {
+                System.out.println("HTTP IO Exception.");
+                e.printStackTrace();
+            }
+
+            try {
+                JSONArray jsonArray = new JSONArray(temp1);
+                reply = jsonArray.getString(0);
+            } catch (JSONException e) {
+                System.out.println("Error in JSON decoding");
+                e.printStackTrace();
+            }
+            return reply;
+        }
+
+        @Override
+        protected void onPostExecute(String res) {
+            System.out.println("connected");
+        }
+    }
+
+    protected class httpQuery extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strs)
+        {
+            HttpClient client=new DefaultHttpClient();
+            HttpPost val=new HttpPost("http://shallowBlue");
+            String returnVal="";
+            ArrayList<NameValuePair> nameValuePairs = new ArrayList<>();
+            nameValuePairs.add(new BasicNameValuePair("tag","shallowBlue"));
+            try
+            {
+                UrlEncodedFormEntity httpEntity = new UrlEncodedFormEntity(nameValuePairs);
+                val.setEntity(httpEntity);
+                HttpResponse response = client.execute(val);
+                String temp = EntityUtils.toString(response.getEntity());
+                JSONArray jsonArray = new JSONArray(temp);
+                returnVal=jsonArray.getString(2);
+
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            return returnVal;
+        }
+        @Override
+        protected void onPostExecute(String res)
+        {
+            System.out.println("get");
+        }
     }
 
 
